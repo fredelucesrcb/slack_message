a6096f673a5a32ee500857eb785f6bbad91bcbca
Roy Fredeluces <fredeluces.rcb@gmail.com>
2025-04-01 21:52:35 +0800

    changed condition in returning success status

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,14 +5,14 @@ export async function GET() {
     return Response.json(process.env.SLACK_WEBHOOK_URL);
 }
 
-export async function POST(request: Request) {
+export async function POST(request: Request,) {
 
     const req = await request.json();
 
     const res = await sendMessageToSlack(req);
 
-    if (res){
-        return NextResponse.json({"message": "success"},{status: 200});
+    if (res.status == 200){
+        return NextResponse.json({"message": "success"}, {status: 200});
     } else {
         return NextResponse.json({"error": "Internal Server Error"}, {status: 404});
     }
